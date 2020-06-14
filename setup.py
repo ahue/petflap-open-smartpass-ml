8ac3c85036c0a2fa2b348a4a043711bd7dd9e64f
A. Hue <ahue87@gmail.com>
2020-06-14 13:38:09 +0200

    Renamed to smartpass

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pfo_ppredict',
-    version='0.1',
+    name='pfo_smartpass',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -14,7 +14,7 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        patpred=pfo_ppredict.main:cli
+        smartpass=pfo_smartpass.main:cli
     ''',
 )
 
